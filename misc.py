f6ab9dd020a98d2d8105df5eb5fa7e6feb71c720
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-03-05 23:01:59 +0500

    misc.py

--- misc.py
+++ misc.py
@@ -12,7 +12,13 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
+        url = self.base_url + 'Finance/Countries'
+        headers={"X-Api-Key" : api_key}
+        response=requests.get(url, headers=headers)
+        if response.status_code==200:
+            return response.json()
+        
+        return False
     
     def get_random_address(self, api_key: str, number: int, culture='en') -> list:
         '''get available misc cultures
@@ -25,4 +31,21 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        url = self.base_url + 'Finance/Vat/Validator'
+        
+        payload = {
+                "country" : culture
+            }
+        headers = {
+                "vat": number
+            }
+        headers = {
+                "X-Api-Key" : api_key
+        }
+        response = requests.get(url, params=payload, headers=headers)
+        if response.status_code==200:
+            return response.json()
+        
+        return False
+s=Misc()
+print(s.get_random_address("2d794c6f46094ceb96bd719c1c26c984"))
