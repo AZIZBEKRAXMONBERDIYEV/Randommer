c4b6a0582fcb165f43a645b380e693a3558b0040
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-03-05 23:01:30 +0500

    finance.py

--- finance.py
+++ finance.py
@@ -11,7 +11,10 @@ class Finance(Randommer):
         Returns:
             list: list of types
         '''
-        pass
+        url = self.base_url + 'Finance/CryptoAddress/Types'
+        headers={"X-Api-Key" : api_key}
+        response=requests.get(url, headers=headers)
+        return response.json()
 
     def get_crypto_address(self, crypto_type: str, api_key: str) -> dict:
         '''get available crypto address
@@ -23,7 +26,19 @@ class Finance(Randommer):
         Returns:
             dict: crypto address
         '''
-        pass
+        url = self.base_url + 'Finance/CryptoAddress'
+        
+        payload = {
+                "cryptotype": crypto_type
+            }
+        headers = {
+                "X-Api-Key": api_key
+            }
+        response = requests.get(url, params=payload, headers=headers)
+        if response.status_code==200:
+            return response.json()
+        
+        return False
 
     def get_countries(self, api_key: str) -> list:
         '''get available countries
@@ -34,7 +49,10 @@ class Finance(Randommer):
         Returns:
             list: crypto address
         '''
-        pass
+        url = self.base_url + 'Finance/CryptoAddress/Types'
+        headers={"X-Api-Key" : api_key}
+        response=requests.get(url, headers=headers)
+        return response.json()
 
     def get_iban_by_country_code(self, country_code: str, api_key: str) -> dict:
         '''get available countries
@@ -46,4 +64,19 @@ class Finance(Randommer):
         Returns:
             dict: idan data
         '''
-        pass
+        url = self.base_url + 'Finance/Iban/{countrycode}'
+        
+        payload = {
+                "crcountrycode": country_code
+            }
+        headers = {
+                "X-Api-Key": api_key
+            }
+        response = requests.get(url, params=payload, headers=headers)
+        if response.status_code==200:
+            return response.json()
+        
+        return False
+
+s=Finance()
+print(s.get_countries("2d794c6f46094ceb96bd719c1c26c984"))
